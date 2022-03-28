6bf113536ec0a02ab6bd425d017ad8951ebf5b67
HYUCK7 <iqeq12191219@gmail.com>
2022-03-28 17:47:01 +0900

    first commit

---
+++ soccer-next/pages/basic/todo.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Todo(){
+    return(
+        <div className="Todo">
+            <input type="checkbox" id="todo0" name="todo0" value="todo0"/>
+            <label for="todo0">Todo 컴포넌트 만들기</label>
+        </div>
+    )
+}
