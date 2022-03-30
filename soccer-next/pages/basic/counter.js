8be153003997eafe802a3b3c4c81a62e57457333
HYUCK7 <iqeq12191219@gmail.com>
2022-03-30 18:35:18 +0900

    2022-03-30 18:25 5

--- soccer-next/pages/basic/counter.js
+++ soccer-next/pages/basic/counter.js
@@ -1,7 +1,13 @@
 import React, {useState} from "react";
+import {useSelector,  useDispatch} from 'react-redux'
+import styles from 'styles/counter.module.css';
 
 export default function Counter() {
-  const [count, setCount] = useState(0)
+  const count = useSelector(selectCount)
+  const dispatch = useDispatch()
+  const[incrementAmount, setIncrementAmount] = useState('0')
+  const incrementValue = Number(incrementAmount) || 0
+
   return (<>
     <button onClick={() => setCount(count+1)}> 더하기 </button>
     <button onClick={() => setCount(count-1)}> 빼기 </button>
